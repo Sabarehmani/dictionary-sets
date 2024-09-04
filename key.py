402bdb107b203a8fc1854f94509ab559da14f991
Sabarehmani <sabarehmani287@gmail.com>
2024-09-04 09:21:23 -0700

    sets

--- key.py
+++ key.py
@@ -1,8 +1,10 @@
 info = {
     "key" : "value",
     "name" : "sabarehmani",
+    "subjects" : ["python", "C", "java"],
     "learning" : "coding",
     "age" : 22,
+    "is_adult" : "true",
     "marks" : 34.5,
 }
 print =(info)
