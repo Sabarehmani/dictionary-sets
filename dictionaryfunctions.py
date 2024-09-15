865103aee184b65b992e75f67a68f6a7cc6c6a0c
Sabarehmani <sabarehmani287@gmail.com>
2024-09-15 09:48:52 -0700

    functions

--- dictionaryfunctions.py
+++ dictionaryfunctions.py
@@ -7,5 +7,17 @@ student = {
      
     }
 }
+print(len(student))
+print(len(list(student.keys())))
 
-print(list(student.keys()))
+student = {
+    "name" : "saba rehmani",
+    "subjects" : {
+        "phy" : 65,
+        "chem" : 23,
+        "maths" : 89,
+     
+    }
+}
+
+print(student.values())
